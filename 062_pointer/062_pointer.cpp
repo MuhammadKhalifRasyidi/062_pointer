b2ba542da40ee25bc08c14310b20da6265d18e37
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-05-10 11:07:48 +0700

    int main

--- 062_pointer/062_pointer.cpp
+++ 062_pointer/062_pointer.cpp
@@ -11,3 +11,19 @@ public:
 void mahasiswa::showNim() { //implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
 }
+
+int main()
+{
+	mahasiswa mhs{ 1 };			//object mhs
+	mhs.showNim();				//member acces operator
+
+	mahasiswa& ref = mhs;		//pointer referencce refMhs
+	ref.nim = 3;				//member acces operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs;		//pointer deference pMhs
+	pMhs->nim = 3;				//arrow operator
+	pMhs->showNim();
+	system("pause");
+	return 0;
+}
