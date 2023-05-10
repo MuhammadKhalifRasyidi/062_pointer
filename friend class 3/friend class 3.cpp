6e0e341f0c149f49cf42a93e2c970fe0149fd84e
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2023-05-10 13:52:36 +0700

    class pelajar 2 {}

--- friend class 3/friend class 3.cpp
+++ friend class 3/friend class 3.cpp
@@ -8,3 +8,13 @@ class manusia {
 public:
 	void showNilaipelajar(pelajar&);
 };
+
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaipelajar(pelajar& x);
+};
